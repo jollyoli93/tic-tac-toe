9e2c876de97568255d9a9dd347b3f0528f2bbda4
Lizzie Kimber <jackoliver1993@yahoo.co.uk>
2023-12-28 12:16:58 +0000

    game board and players added

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-console.log("Hello")
+function gameBoard(){
+    // 3 x 3 square 
+    size = 3;
+    const board = [];
+
+    for (let i=0; i<size; i++){
+        board[i] = [];
+        for (let j=0; j<size; j++){
+            board[i].push('cell()')
+        }
+    };
+
+    return board;
+};
+
+// console.log(gameBoard())
+
+function player(){
+    let score = 0;
+
+    const player1 = "Player One";
+    const player2 = "Player Two";
+
+    const players = [
+        {
+            name: player1,
+            token: 'x',
+        },
+        {
+            name: player2,
+            token: 'o',
+
+        }
+    ];
+
+    return players;
+};
+
+const player1 = player()[0];
+const player2 = player()[1];
+
+console.log(player2);
+
+function game(){
+
+};
+
+// const {row1} = gameBoard();
+// console.log(row1[0]);
